5f8ab55090c2da1c98b2bb2d26dcec472f9a905f
ianteodoros <ianteodoros@zohomail.eu>
2025-06-17 12:47:17 +0300

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1 style={{ color: '#2e7d32' }}>BlogMachine</h1>
+      <p>Generăm text pentru blog în câteva secunde.</p>
+      <a href="/app">Mergi la generator</a>
+    </div>
+  );
+}
